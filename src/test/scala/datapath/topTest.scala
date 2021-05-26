93b38ac0a27fd63ad10b292de5086c10fa934784
shahzaib <shahzaibceo@gmail.com>
2021-05-27 04:13:53 +0500

    Tests Abandoned

--- src/test/scala/datapath/topTest.scala
+++ src/test/scala/datapath/topTest.scala
@@ -1,18 +1,18 @@
-package datapath
+// package datapath
 
-import org.scalatest._ 
-import chisel3._
-import chisel3.util._ 
-import chiseltest._
-import chiseltest.experimental.TestOptionBuilder._ 
-import chiseltest.internal.VerilatorBackendAnnotation
+// import org.scalatest._ 
+// import chisel3._
+// import chisel3.util._ 
+// import chiseltest._
+// import chiseltest.experimental.TestOptionBuilder._ 
+// import chiseltest.internal.VerilatorBackendAnnotation
 
-class topTest extends FreeSpec with ChiselScalatestTester {
-    "TOP Test" in {
-        test(new Top).withAnnotations(Seq(VerilatorBackendAnnotation)){ c =>
+// class topTest extends FreeSpec with ChiselScalatestTester {
+//     "TOP Test" in {
+//         test(new Top).withAnnotations(Seq(VerilatorBackendAnnotation)){ c =>
 
-        // c.clock.step(50)
+//         c.clock.step(50)
 
-        }
-    }
-}
+//         }
+//     }
+// }
