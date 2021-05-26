eae88bbf99a2cf8ec8a780a94a3f94dab5e588b3
shahzaib <shahzaibceo@gmail.com>
2021-05-27 02:55:41 +0500

    Integrating Tilelink in Main Memory

--- src/main/scala/datapath/MainMem.scala
+++ src/main/scala/datapath/MainMem.scala
@@ -1,6 +1,7 @@
 package datapath
 
 import chisel3._
+import tilelink._ 
 //import chisel3.util.experimental.loadMemoryFromFile
 
 class MainMem extends Module{
@@ -23,19 +24,36 @@ class MainMem extends Module{
 	//loadMemoryFromFile(dMem, "/home/hellcaster/Memory.txt")
 	*/
 
-	val mem = Mem(1024,SInt(32.W))
-	when(io.ld === 1.U){
-		io.DataOut := mem(io.Address)
-	}.otherwise{
-		io.DataOut := DontCare
-	}
+
+	val tLink = Module(new TileTop())
+
+	tLink.io.channelA.valid := 1.B
+	tLink.io.channelA.bits.a_param := 0.U
+	tLink.io.channelA.bits.a_size := 2.U
+	tLink.io.channelA.bits.a_source := 2.U
+	tLink.io.channelA.bits.a_mask := 1.U
+	tLink.io.channelA.bits.a_corrupt := 0.U
+	tLink.io.channelA.bits.a_opcode := Mux(io.ld === 1.U, 4.U, Mux(io.str === 1.U, 1.U, 2.U))
+	tLink.io.channelA.bits.a_address := io.Address
+	tLink.io.channelA.bits.a_data := Mux(io.str === 1.U, io.DataIn.asUInt, 0.U)
+
+	tLink.io.channelD.ready := 1.B
+
+	io.DataOut := tLink.io.channelD.bits.d_data.asSInt
+
+	// val mem = Mem(1024,SInt(32.W))
+	// when(io.ld === 1.U){
+	// 	io.DataOut := mem(io.Address)
+	// }.otherwise{
+	// 	io.DataOut := DontCare
+	// }
 
 	
-	when(io.str === 1.U){
-		mem(io.Address) := io.DataIn
-	}.otherwise{
-		io.DataOut := DontCare
-	}
+	// when(io.str === 1.U){
+	// 	mem(io.Address) := io.DataIn
+	// }.otherwise{
+	// 	io.DataOut := DontCare
+	// }
 
 }
 
